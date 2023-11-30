605e2b2558d0a0dafcfbc890516ad4de9a05356f
hockeyiscool19 <jordaneisenman@gmail.com>
2023-11-30 02:16:18 -0500

    stuff

--- tests.py
+++ tests.py
@@ -1,13 +1,20 @@
 import os
+
 import pytest
 
 # Access the environment variable
 foo = os.getenv('FOO', 'Default Value')
 
+bar = os.getenv("BAZZ", None)
+
+if bar not None:
+    print("Got secret")
+else:
+    print("Oh no")
 def test_foo_env_variable():
     # Your test here
     assert foo == "Bar"  # Replace "Bar" with the expected value of FOO
 
 def test_sample():
     assert 2 == 2
-r
+
